c8c1493136f0c9f3434562f6acab7e37ef56b958
DookieCookie <harshvardhan.gururani.cd.phy22@itbhu.ac.in>
2026-02-11 12:01:07 +0530

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const webhookController = require('../controllers/webhook');
 
+// Gupshup often requires a GET request to verify the URL, and a POST to send the actual data
+router.get('/webhook', webhookController.verifyWebhook);
+router.post('/webhook', webhookController.receiveMessage);
+
+module.exports = router;
